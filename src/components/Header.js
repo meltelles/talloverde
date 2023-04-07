054e1e25c459af23f90cf3ddaf0586e6d228f432
Melany Telles <melanytelles@gmail.com>
2023-04-07 12:10:06 -0300

    Arme Navbar dentro del Header, incorpore bootstrap

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar"
+
+const Header = () => {
+    return (
+      <header className="navbar">
+        <h1 className="navbar__title">Tallo verde</h1>
+        <Navbar />
+      </header>
+    );
+  }
+  
+  export default Header
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar__list">
+          <a href="#" className="navbar__link link">Nosotros</a>
+          <a href="#" className="navbar__link link">Productos</a>
+          <a href="#" className="navbar__link link">Ayuda</a>
+        </nav>
+    );
+  }
+  
+  export default Navbar
