cb5372b8fcb2e6bcf6c7a063c903ae984e0b9771
Melany Telles <melanytelles@gmail.com>
2023-04-10 19:32:57 -0300

    Agregue componente list container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
-import Navbar from "./components/Navbar"
+import ItemListContainer from "./components/ItemListContainer";
 
 function App() {
   return (
     <>
     <Header />
+    <ItemListContainer greeting={"Bienvenidos"} />
     </>
   );
 }
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,7 @@
+export default function ItemListContainer({ greeting }) {
+    return (
+        <div>
+            {greeting}
+        </div>
+    );
+}
